65cb0942c0949edeb7f5bd638c3da35c8c9b86f6
NathaRHS <sylvanonath@gmail.com>
2026-06-12 11:08:21 +0300

    rajout demo nearby

--- src/data/projets.js
+++ src/data/projets.js
@@ -28,7 +28,7 @@ export const PROJETS = [
     role: "Développeur front-end",
     img: "https://images.unsplash.com/photo-1556742049-0cfed4f6a45d?w=1200&q=80",
     github: "https://github.com",
-    demo: "",
+    demo: "nearbyshop.me",
   },
   {
     slug: "schooltech",
